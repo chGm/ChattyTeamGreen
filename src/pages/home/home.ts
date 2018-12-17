1900433a5c4200b97ba5e633d7fbddccb5d36532
Milo Tolboom <milotolboom@gmail.com>
2018-12-17 10:36:39 +0100

    socket

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as io from 'socket.io-client';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  socket: any;
 
   constructor(public navCtrl: NavController) {
-
   }
 
+  ionViewDidLoad() {
+      this.socket = io("https://smog.memelord.nl");
+
+      this.socket.on('connect', () => {
+        console.log("connected");
+      })
+  }
 }
