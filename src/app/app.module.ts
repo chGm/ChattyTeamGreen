70a3dd4d50fb1852c2eee2dfc36f8d84060299cf
Milo Tolboom <milotolboom@gmail.com>
2018-12-17 11:26:09 +0100

    stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {Shake} from "@ionic-native/shake";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { BackendProvider } from '../providers/backend/backend';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {BackendProvider} from '../providers/backend/backend';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    BackendProvider
-  ]
+    declarations: [
+        MyApp,
+        HomePage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage
+    ],
+    providers: [
+        StatusBar,
+        Shake,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler},
+        BackendProvider
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,54 +1,75 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 import * as io from 'socket.io-client';
+import {Shake} from "@ionic-native/shake";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
-  socket: any;
-  username: string;
-  messages: Message[] = [];
-  myMessage: string;
-  constructor(public navCtrl: NavController) {}
+    socket: any;
+    username: string;
+    messages: Message[] = [];
+    myMessage: string;
 
-  ionViewDidLoad() {
-    this.socket = io('https://smog.memelord.nl');
+    isBuzzing: boolean = false;
 
-    this.socket.on('connect', () => {
-      console.log('connected');
-    });
+    constructor(public navCtrl: NavController, private shake: Shake) {
+    }
 
-    this.socket.on('username', (username: string) => {
-      this.username = username;
-    });
+    ionViewDidLoad() {
+        this.setupSocket();
 
-    this.socket.on('msg', (message: string, username: string) => {
-      this.messages.push(new Message(username, message, false));
-    });
-  }
+        const watch = this.shake.startWatch(60).subscribe(() => {
+            this.socket.emit('buzzer');
+            this.buzz();
+        });
+    }
 
-  sendBuzzer() {
-    // TODO: fill method
-  }
+    setupSocket() {
+        this.socket = io('https://smog.memelord.nl');
 
-  sendMessage() {
-    if (this.myMessage === '') return;
-    this.socket.emit('msg', this.myMessage, this.username);
-    this.messages.push(new Message(this.username, this.myMessage, true));
-    this.myMessage = '';
-  }
+        this.socket.on('connect', () => {
+            console.log('connected');
+        });
+
+        this.socket.on('username', (username: string) => {
+            this.username = username;
+        });
+
+        this.socket.on('msg', (message: string, username: string) => {
+            this.messages.push(new Message(username, message, false));
+        });
+
+        this.socket.on('buzzer', () => {
+            this.buzz();
+        });
+    }
+
+    buzz() {
+        this.isBuzzing = true;
+        setTimeout(() => {
+            this.isBuzzing = false;
+        }, 1000);
+    }
+
+    sendMessage() {
+        if (this.myMessage === '') return;
+        this.socket.emit('msg', this.myMessage, this.username);
+        this.messages.push(new Message(this.username, this.myMessage, true));
+        this.myMessage = '';
+    }
 }
 
 class Message {
-  username: string;
-  message: string;
-  sent: boolean;
+    username: string;
+    message: string;
+    sent: boolean;
 
-  constructor(username: string, message: string, sent: boolean) {
-    this.username = username;
-    this.message = message;
-    this.sent = sent;
-  }
+    constructor(username: string, message: string, sent: boolean) {
+        this.username = username;
+        this.message = message;
+        this.sent = sent;
+    }
 }
