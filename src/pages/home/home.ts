a0baf5c6a40c65e584191f7d9db0ee78e6357509
chGm <chman@avans.nl>
2018-12-17 10:50:13 +0100

    no message

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,15 +8,43 @@ import * as io from 'socket.io-client';
 })
 export class HomePage {
   socket: any;
-
-  constructor(public navCtrl: NavController) {
-  }
+  username: string;
+  messages: [Message];
+  constructor(public navCtrl: NavController) {}
 
   ionViewDidLoad() {
-      this.socket = io("https://smog.memelord.nl");
+    this.socket = io('https://smog.memelord.nl');
 
-      this.socket.on('connect', () => {
-        console.log("connected");
-      })
+    this.socket.on('connect', () => {
+      console.log('connected');
+    });
+
+    this.socket.on('username', (username: string) => {
+      this.username = username;
+    });
+
+    this.socket.on('msg', (message: string, username: string) => {
+      this.messages.push(new Message(username, message, false));
+    });
+  }
+
+  sendBuzzer() {
+    // TODO: fill method
+  }
+
+  sendMessage(message: string) {
+    this.socket.emit('msg', message, this.username);
+  }
+}
+
+class Message {
+  username: string;
+  message: string;
+  sent: boolean;
+
+  constructor(username: string, message: string, sent: boolean) {
+    this.username = username;
+    this.message = message;
+    this.sent = sent;
   }
 }
